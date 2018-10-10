ec058e9822614c01a48c02e381e06499cea987f0
Serge Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2018-10-10 12:21:46 +0300

    lection_7

--- main.js
+++ main.js
@@ -1,105 +1,76 @@
 // 1
-console.log('#1');
-function fillArray() {
-    let n = +prompt("n = ?","");
-    let m = +prompt("m = ?","");
-    let A = [];
-    A.length = n;
-    for (i = 0; i < A.length; i++) {
-        A[i] = new Array(m);
+console.log("#1");
+function showArrayCentralElements(Arr) {
+    var CentralElements = [];
+    if (Arr.length % 2 == 0) {
+        CentralElements.push(Arr[Arr.length / 2 - 1]);
+        CentralElements.push(Arr[Arr.length / 2]);
+    } else {
+        CentralElements.push(Arr[Math.ceil((Arr.length - 1) / 2)]);
     }
-    for (i = 0; i < A.length; i++) {
-        for (j = 0; j < A[i].length; j++) {
-            A[i][j] = Math.floor(Math.random() * 90 + 10);
-        }
-    }
-    return A;
+    return CentralElements;
 }
-console.log(fillArray());
+console.log(showArrayCentralElements([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+console.log(showArrayCentralElements([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 // 2
-console.log('#2');
-let A = [2, 5, "str", 6, 7, 43, "str", 9, "str",];
-let B = ["str", 5, 56, 3, 78, 24, "str", 4, 8, "str",];
-function compareArray(A, B) {
-    let sumA = 0;
-    for (i = 0; i < A.length; i++) {
-        if (typeof A[i] == "number") {
-            sumA += A[i];
+console.log("#2");
+function doFunction(x, y, callback) {
+    let counter = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] == "number") {
+            counter++;
         }
     }
-    let sumB = 0;
-    for (i = 0; i < B.length; i++) {
-        if (typeof B[i] == "number") {
-            sumB += B[i];
-        }
+    if (counter == 1) {
+        console.log("counter: " + counter);
+        return factorial(x);
     }
-    if (sumA > sumB) {
-        return A;
-    }   else {
-        return B;
-    }
+        return callback(x, y);
 }
-console.log(compareArray(A, B));
-// 3
-console.log('#3');
-let x = +prompt('x = ?','');
-let y = +prompt('y = ?','');
-let znak = prompt('znak = ?','');
-function doMath(x, znak, y) {
-    var res = 0;
-    switch (znak) {
-        case '+': 
-        res = x + y;
-        break;
-        case '-': 
-        res = x - y;
-        break;
-        case '*': 
-        res = x * y;
-        break;
-        case '/': 
-        res = x / y;
-        break;
-        case '%': 
-        res = x % y;
-        break;
-        case '^': 
-        res = x ^ y;
-        break;
+function power(x, y) {
+    let res = x;
+    for (var i = 1; i < y; i++) {
+        res *=x;
     }
     return res;
 }
-console.log(doMath(x, znak, y));
-// 4 
-console.log('#4');
-let str = "hello world";
-function removeLetters(l1, l2) {
-    // var l1;
-    // var l2;
-    A = [];
-    B = [];
-    A = str.split('');
-    for (i = 0; i < A.length; i++) {
-        if (A[i] !== l1 && A[i] !== l2) {
-            B.push(A[i]);
-        }
-    }
-    return B.join('');
+console.log(doFunction(2, 3, power));
+// 3
+console.log("#3");
+function sum(x, y) {
+    return x + y;
 }
-console.log(removeLetters("l","d"));
+function div(x, y) {
+    return x / y;
+}
+function mul(x, y) {
+    return x * y;
+}
+console.log(doFunction(16, -23, mul));
+// 4
+console.log("#4");
+function factorial(x) {
+    return (x != 1) ? x * factorial(x - 1) : 1;
+}
+console.log(doFunction(6, factorial));
 // 5
-console.log('#5');
-A = [4, 23, 5, 64, 78, 4, 62, 7];
-function filter(Arr, callback) {
-    var newArr = [];
-    for (var i = 0; i < Arr.length; i++) {
-        if (callback(Arr[i]) == true) {
-            newArr.push(Arr[i]);
+console.log("#5");
+var Some = Factory([[2, 7], [3, 9, 6], [4], [2, 2, 2], [1]]);
+function Factory(list) {
+    let counter = 0;
+    return function() {// Some
+        counter++
+        let arrayNumber = counter;
+        return function() {//struct function
+            return list[arrayNumber];;
         }
     }
-    return newArr;
 }
-function isEven(x) {
-    return x % 2 == 0;
-}
-console.log(filter(A, isEven));
+var struct_1 = Some(); 
+var struct_2 = Some(); 
+var struct_3 = Some(); 
+console.log(struct_1());
+console.log(struct_1());
+console.log(struct_1());
+console.log(struct_2());
+console.log(struct_3());
