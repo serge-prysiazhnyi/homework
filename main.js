44aec539e61d99e3a4007a2d4cc1fdd5df52a964
Serge Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2018-10-24 09:57:36 +0300

    lection_14

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
 window.onload = function() {
-    this.setInterval(function(){
-        var myTextArea = document.querySelector("#myTextArea");
-        var inputs = document.querySelectorAll("input");
-        var text = "";
-        for(var i = 0; i < inputs.length; i++) {
-            if(inputs[i].value) {
-                if(i === inputs.length - 1) {
-                    text += inputs[i].value;
-                }   else {
-                text += inputs[i].value + "," + " ";
-                }
-            }
+    var value = 0;
+    var commission = 0;
+    function getValue(){
+        value = +this.value;
+        document.querySelector('#value').innerHTML = value;
+        setValueBlockHeight(this.value);
+        commission = calcCommission(value);
+        this.setCommissionBlockHeight = setCommissionBlockHeight;
+        this.setCommissionBlockHeight();
+    }
+    function setValueBlockHeight(value){
+        valueBlock.style.height = (value * 2) + 'px';
+    }
+    function calcCommission(){
+        if(value < 20) {
+            return Math.round((value / 100) * 2);
         }
-        myTextArea.value = text;
-    },10);
-};
+        if(value >= 20 && value < 50) {
+            return Math.round((value / 100) * 4);
+        }
+        if(value >= 50 && value < 75){
+            return Math.round((value / 100) * 6);
+        }
+        if(value >= 75 && value <= 100){
+            return Math.round((value / 100) * 8);
+        }
+    }
+    function setCommissionBlockHeight(){
+        commissionBlock.style.height = (commission * 2) + 'px';
+    }
+    var range = document.querySelector('#rangeInput');
+    var text = document.querySelector('#textInput');
+    var valueBlock = document.querySelector('#value-block');
+    var commissionBlock = document.querySelector('#commission-block');
+    range.addEventListener('change', getValue);
+    text.addEventListener('change', getValue);
+    range.addEventListener('change', function(){
+        text.value = value;
+    });
+    text.addEventListener('change', function(){
+        range.value = value;
+    });
+}; 
