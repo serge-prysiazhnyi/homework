e50cafa14967e9fc2c74f3f05c498768c95738b6
Serge Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2018-12-10 17:23:01 +0200

    Lection_25

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-window.onload = function() {
-    var cinemas = document.querySelectorAll('.cinema__item');
-    var struct = [];
-    cinemas.forEach(function(item){
-        struct.push({
-            name: item.children[0].innerHTML,
-            price: item.children[1].innerHTML
-        });
-    })
-    console.log(struct);
-    struct
-    .filter(function(item){
-        return item.name;
-    })
-    .forEach(function(item){
-        console.log(item.name);
-    });
-    var sum = struct
-    .filter(function(item){
-        return item.price;
-    })
-    .map(function(item){
-        return parseInt(item.price);
-    })
-    .reduce(function(sum, current){
-        return sum + current;
-    })
-    console.log(sum);
-};
+let app = new Vue({
+    el: '#app',
+    data: {
+        persons: [
+          {
+            id: 001,
+            title: 'some title',
+            age: 25,
+            adress: 'some adress'
+          },
+          {
+            id: 002,
+            title: 'some title 2',
+            age: 20,
+            adress: 'some adress 2'
+          }
+        ],
+        buffer: {
+          index: null,
+          key: '',
+          value: ''
+        }
+    },
+    methods: {
+      onClick: function(index,key,value){
+        this.buffer.index = index;
+        this.buffer.key = key;
+        this.buffer.value = value;
+      },
+      apply: function(){
+        let index = this.buffer.index;
+        let key = this.buffer.key;
+        let value = this.buffer.value;
+        this.persons[index][key] = value;
+      }
+    }
+})
